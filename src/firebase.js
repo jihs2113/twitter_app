5554c8cef4bae35d6de8f1b0c8bbd1f5eb90c2a2
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-05-26 23:17:47 +0900

    firebase setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEPgUUYbX2uaMu8dXhYdtOH_qpP5Xhx1k",
+    authDomain: "twitterapp-44109.firebaseapp.com",
+    projectId: "twitterapp-44109",
+    storageBucket: "twitterapp-44109.appspot.com",
+    messagingSenderId: "567844713569",
+    appId: "1:567844713569:web:a1be4d2e7ae22b9c17a620"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
