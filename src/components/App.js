c17be420d09133ad7eed00ffe5a2bc4353536691
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-05-30 19:23:10 +0900

    auth setting & log form

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useState } from 'react';
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
-function App() {
-  return <AppRouter/>;
+function App() {  
+
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  //user를 불러와 authentication을 지정해준다.
+  return ( 
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>;
+      <footer>&copy; {new Date().getFullYear()} twitter_app </footer>
+    </>
+ );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) =>{
+        // console.log(event.target.name);
+        const{
+            target: {name, value},
+        } =event;
+        if (name === "email"){
+            setEmail(value);
+        }else if(name ==="password"){
+            setPassword(value);
+        }
+    };
+    const onSubmit = (event) =>{
+        event.preventDefault();
+    };
+    return (
+    <div>    
+        <form>
+            <input 
+                name="email" 
+                type="text" 
+                placeholder="Email" 
+                required value={email} 
+                onChange={onChange}
+            />
+            <input 
+                name="password" 
+                type="password" 
+                placeholder="password" 
+                required value={password}
+                onChange={onChange}
+            />
+            <input 
+                type="submit" 
+                placeholder="Log In" 
 
+            />
+        </form>
+        <div>
+            <button onClick="">Google</button>
+            <button>Github</button>
+        </div>
+    </div>
+    );
+};
  export default Auth;
