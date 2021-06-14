ff4f3112ff04d871b2f52add0aaa4841f5f36f0c
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-06-14 20:50:54 +0900

    tweet create read success

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,41 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    //collection을 get하는 state
+    const getNweets = async() =>{
+        //collection을 get하는 함수
+        const dbNweets = await dbService.collection("nweet").get(); 
+        // dbNweets.forEach((document) => console.log(document.data()));
+        //내 state에 있는 document.data를 다루고있음.
+        dbNweets.forEach((document) => {
+            //dbNweets 안에 있는 모든 document를 다루려면 set을 쓴다.
+            const nweetObject ={
+                ...document.data(),
+                //이건 모든 document의 data다.
+                id: document.id,
+            };
+            // setNweets((prev) => [document.data(), ...prev]);
+            //모든 이전 nweets에 대해 배열을 리턴함.
+            //새로 작성한 트윗과 그 이전것들을 리턴.
+            //set이붙는 함수는 값대신 함수를 전달할수있음.
+            //함수를 전달하면 리액트는 이전 값에 접근하게해줌.
+            setNweets((prev) => [nweetObject, ...prev]);
+            //위에 document.data를 보기쉽게 객체로 만들어준다.
+            //data와 id가 있음.
+
+           
+        });
+         //dbnweets 이전 document들에게 setNweet에서 함수를 사용중인데
+         //배열을 리턴중이다.
+         //배열은 최근 document 그뒤로 이전 document를 붙인다.
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
     const onSubmit = async (event) =>{
         //collection.add가 promise를 리턴하므로 async 추가
         event.preventDefault();
@@ -24,6 +56,7 @@ const Home = () => {
         } =event;
         setNweet(value);
     };
+    console.log(nweets);
     return(
 
     <div>
@@ -31,6 +64,13 @@ const Home = () => {
             <input value={nweet} onChange={onChange} type="text" placeholder=" whats on your mind?" maxLength={120}/>
             <input onChange={onChange} type="submit" value="Nweet"/>
         </form>
+        <div>
+            {nweets.map((nweet) =>( 
+            <div key={nweet.id}>
+                <h4>{nweet.nweet}</h4>
+            </div>
+            ))};
+        </div>
     </div>
     );
 }
